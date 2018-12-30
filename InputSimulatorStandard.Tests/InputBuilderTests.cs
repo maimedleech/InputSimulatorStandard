63622779b4b6576fa74479f8a0e186a86588ccf6
Gregor Sindl <gregor4005@gmail.com>
2018-12-30 13:40:08 +0100

    Rename project

---
+++ InputSimulatorStandard.Tests/InputBuilderTests.cs
@@ -0,0 +1,20 @@
+﻿namespace InputSimulatorStandard.Tests
+{
+    using Native;
+
+    using Xunit;
+
+    public class InputBuilderTests
+    {
+        [Fact]
+        public void AddKeyDown()
+        {
+            var builder = new InputBuilder();
+            Assert.Empty(builder.ToArray());
+            builder.AddKeyDown(VirtualKeyCode.VK_A);
+            Assert.Single(builder);
+            Assert.Equal((uint)InputType.Keyboard, builder[0].Type);
+            Assert.Equal((ushort)VirtualKeyCode.VK_A, builder[0].Data.Keyboard.KeyCode);
+        }
+    }
+}
---
+++ InputSimulatorStandard/Native/MouseKeyboardHardwareInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.InteropServices;
+
+namespace InputSimulatorStandard.Native
+{
+    /// <summary>
+    /// The combined structure that includes Mouse, Keyboard and Hardware Input message data (see: http://msdn.microsoft.com/en-us/library/ms646270(VS.85).aspx)
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    internal struct MouseKeyboardHardwareInput
+    {
+        /// <summary>
+        /// The <see cref="MouseInput"/> definition.
+        /// </summary>
+        [FieldOffset(0)]
+        public MouseInput Mouse;
+
+        /// <summary>
+        /// The <see cref="KeyboardInput"/> definition.
+        /// </summary>
+        [FieldOffset(0)]
+        public KeyboardInput Keyboard;
+
+        /// <summary>
+        /// The <see cref="HardwareInput"/> definition.
+        /// </summary>
+        [FieldOffset(0)]
+        public HardwareInput Hardware;
+    }
+}
