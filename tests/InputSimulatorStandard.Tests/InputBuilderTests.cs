92a88cb63d94abdd3831d6372212b58b67f00bd6
Gregor Sindl <gregor4005@gmail.com>
2018-12-31 23:04:05 +0100

    Extend InputBuilderTests

--- tests/InputSimulatorStandard.Tests/InputBuilderTests.cs
+++ tests/InputSimulatorStandard.Tests/InputBuilderTests.cs
@@ -1,20 +1,69 @@
 ﻿namespace InputSimulatorStandard.Tests
 {
+    using System.Collections.Generic;
+
     using Native;
 
     using Xunit;
 
     public class InputBuilderTests
     {
-        [Fact]
-        public void AddKeyDown()
+        private readonly InputBuilder inputBuilder;
+
+        public InputBuilderTests()
         {
-            var builder = new InputBuilder();
-            Assert.Empty(builder.ToArray());
-            builder.AddKeyDown(VirtualKeyCode.VK_A);
-            Assert.Single(builder);
-            Assert.Equal((uint)InputType.Keyboard, builder[0].Type);
-            Assert.Equal((ushort)VirtualKeyCode.VK_A, builder[0].Data.Keyboard.KeyCode);
+            this.inputBuilder = new InputBuilder();
+        }
+
+        public class GetEnumeratorMethod : InputBuilderTests
+        {
+            [Fact]
+            public void ReturnsIEnumerator()
+            {
+                var result = this.inputBuilder.GetEnumerator();
+                Assert.IsAssignableFrom<IEnumerator<Input>>(result);
+            }
+        }
+
+        public class IsExtendedKeyMethod : InputBuilderTests
+        {
+            [Theory]
+            [InlineData(VirtualKeyCode.MENU)]
+            [InlineData(VirtualKeyCode.RMENU)]
+            [InlineData(VirtualKeyCode.CONTROL)]
+            [InlineData(VirtualKeyCode.RCONTROL)]
+            [InlineData(VirtualKeyCode.INSERT)]
+            [InlineData(VirtualKeyCode.DELETE)]
+            [InlineData(VirtualKeyCode.HOME)]
+            [InlineData(VirtualKeyCode.END)]
+            [InlineData(VirtualKeyCode.PRIOR)]
+            [InlineData(VirtualKeyCode.NEXT)]
+            [InlineData(VirtualKeyCode.RIGHT)]
+            [InlineData(VirtualKeyCode.UP)]
+            [InlineData(VirtualKeyCode.LEFT)]
+            [InlineData(VirtualKeyCode.DOWN)]
+            [InlineData(VirtualKeyCode.NUMLOCK)]
+            [InlineData(VirtualKeyCode.CANCEL)]
+            [InlineData(VirtualKeyCode.SNAPSHOT)]
+            [InlineData(VirtualKeyCode.DIVIDE)]
+            public void ReturnsTrue(VirtualKeyCode keyCode)
+            {
+                var result = InputBuilder.IsExtendedKey(keyCode);
+                Assert.True(result);
+            }
+        }
+
+        public class AddKeyDownMethod : InputBuilderTests
+        {
+            [Fact]
+            public void AddKeyDown()
+            {
+                Assert.Empty(this.inputBuilder.ToArray());
+                this.inputBuilder.AddKeyDown(VirtualKeyCode.VK_A);
+                Assert.Single(this.inputBuilder);
+                Assert.Equal((uint)InputType.Keyboard, this.inputBuilder[0].Type);
+                Assert.Equal((ushort)VirtualKeyCode.VK_A, this.inputBuilder[0].Data.Keyboard.KeyCode);
+            }
         }
     }
 }
