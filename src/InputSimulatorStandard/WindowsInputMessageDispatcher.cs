276bc44c8e37b3b5198bac643d8a02d62c069511
Gregor Sindl <gregor4005@gmail.com>
2019-08-05 21:17:19 +0200

    Update formatting

--- src/InputSimulatorStandard/WindowsInputMessageDispatcher.cs
+++ src/InputSimulatorStandard/WindowsInputMessageDispatcher.cs
@@ -31,7 +31,7 @@
                 throw new ArgumentException("The input array was empty", nameof(inputs));
             }
 
-            var successful = NativeMethods.SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof (Input)));
+            var successful = NativeMethods.SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(Input)));
             if (successful != inputs.Length)
             {
                 throw new Exception("Some simulated input commands were not sent successfully. The most common reason for this happening are the security features of Windows including User Interface Privacy Isolation (UIPI). Your application can only send commands to applications of the same or lower elevation. Similarly certain commands are restricted to Accessibility/UIAutomation applications. Refer to the project home page and the code samples for more information.");
